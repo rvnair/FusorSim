fb277f37dfcda653892b96eb8ea19712f68d03a6
AdityaAtulTewari <adityaatewari@gmail.com>
2020-03-16 15:10:38 -0500

    Added boiler plate stubs

---
+++ src/two-body/two_body.h
@@ -0,0 +1,7 @@
+#ifndef _two_body_h_
+#define _two_body_h_
+#include "main2b.h"
+
+output* two_body_eval(locate p0, double m0, double q0, locate p1, double m1, double q1, double QE, double r, double dt, double n);
+
+#endif
